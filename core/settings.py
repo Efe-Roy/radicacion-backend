cf324c6dc4f6491460075b158b0cce3e073af982
Efe-Roy <dakaraefe@gmail.com>
2023-05-28 16:59:02 +0100

    nc

--- core/settings.py
+++ core/settings.py
@@ -89,27 +89,27 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DATABASE_NAME'), 
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASS'),
-        'HOST': env('DATABASE_HOST'), 
-        'PORT': env('DATABASE_PORT'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'myprojdb', 
-#         'USER': 'myprojuser',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'localhost', 
-#         'PORT': '5432',
+#         'NAME': env('DATABASE_NAME'), 
+#         'USER': env('DATABASE_USER'),
+#         'PASSWORD': env('DATABASE_PASS'),
+#         'HOST': env('DATABASE_HOST'), 
+#         'PORT': env('DATABASE_PORT'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myprojdb', 
+        'USER': 'myprojuser',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost', 
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
